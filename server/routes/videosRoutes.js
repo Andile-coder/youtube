d7c2ba3bfb300adffdd750a110b027b977b2bffe
Andile-coder <maselaandile@gmail.com>
2021-09-22 17:24:51 +0200

    getRoute

--- server/routes/videosRoutes.js
+++ server/routes/videosRoutes.js
@@ -1,7 +1,7 @@
 const videoController = require("../videoController");
 const express = require("express");
 const router = express.Router();
-// router.get("/", videoController.video_getAll);
+router.get("/", videoController.video_getAll);
 router.post("/", videoController.video_add);
 router.delete("/:id", videoController.video_deleteById);
 router.put("/:id", videoController.video_updateRating);
